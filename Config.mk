3532e6e027e5811a4aff63faac8d3e86eaa1e81a
Pierre Olivier <polivier@vt.edu>
2016-07-21 16:08:50 -0400

    Xen source dir is npw a parameter in Config.mk

--- Config.mk
+++ Config.mk
@@ -5,6 +5,9 @@
 # we do a measurement
 CURRENTLY_RUNNING="0 2 4 8 16 32"
 
+# Xen source dir for unikernel compilation
+XEN_SRC_DIR="/root/popcorn-xen"
+
 # Path to the unikernel binary that will be the currently 
 # running one
 RUNNING_UK="unikernels/background/mini-os.gz"
@@ -28,6 +31,12 @@ BG_BASENAME="mini-os"
 # Time to wait (s) between the boot of the background uks 
 # and the boot of the measured uk
 SAFETYSLEEP=0
+
+# Less important parameters below
+# ------------------------------
+
+# Default basename for background unikernels
+BG_BASENAME="mini-os"
 
 # Name of the measured uk
 MEASURED_NAME="measured"
