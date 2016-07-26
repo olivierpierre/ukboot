91890460e65896a5dbe1c27b140cca056048a234
Pierre Olivier <polivier@vt.edu>
2016-07-25 21:04:13 -0400

    Added cpu usage calibration tool

---
+++ tools/cpu-usage-calibration/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+typedef uint16_t domid_t;
+
+#include <xenbus.h>
+
+#define READ_PATH	"/test/read_access"
+
+int main(void) {
+    char *val, *err;
+    int i;
+
+    val = (char *)malloc(128 * sizeof(char));
+    err = (char *)malloc(128 * sizeof(char));
+    
+    sleep(3);
+
+	err = xenbus_read(XBT_NIL, READ_PATH, &val);
+	if(err != NULL)
+	{
+		printf("Error reading in %s: %s\n", READ_PATH, err);
+		goto out_free;
+	}
+
+    i = atoi(val);
+
+    while(1) {
+        usleep(i);
+    }
+
+out_free:
+	free(val);
+	free(err);
+
+    return 0;
+      
+
+}
---
+++ tools/cpu-usage-calibration/test.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then 
+
+    echo "Usage:  $0 <max num of microsec of sleep()> <num of iterations of xentop>"
+    exit
+fi  
+
+# Initializations of data files
+
+echo -n "" > avg-stddev.dat
+
+for i in `seq 1 $1 `;do 
+    # Prepare the to items in the xenstore we will access
+    xenstore-write /test/read_access $i
+
+    # Give the permission to our VM which will be named "Mini-OS"
+    xenstore-chmod /test/read_access rMini-OS
+
+    # Launch unikernel
+    xl create domain_config.xl -q
+
+    # wait 3 secs for unikernel bootting
+    sleep 3
+
+    # echo passing message
+    echo "test/read_access read from dom0:"
+
+    xenstore-read /test/read_access
+
+    sleep 3
+
+    echo -n "" > cpu-log.dat
+    xentop -b -i $(($2+1)) | grep Mini | awk '{if(NR > 1) {print $4}}' >> cpu-log.dat 
+    awk -v microsec=$i '{if(NR >= 1) {sum += $1; sumsq +=$1*$1}} END {print microsec "," sum/NR "," sqrt(sumsq/NR - (sum/NR)**2)}' cpu-log.dat >> avg-stddev.dat
+    
+    # clean temp file
+    rm cpu-log.dat
+
+    # Cleanup the xenstore
+    xenstore-rm /test
+
+    echo "xenstore path test/ has been cleaned."
+
+    xl shutdown -a
+
+    echo "shutdown guest domain."
+    echo "done" $i/$1
+done
+
+# Plot if gnuplot is installed
+# gp=`which gnuplot`
+#
+# if [ "$gp" == "" ]; then
+#     echo "Gnuplot not detected, no plot."
+#     exit 0
+# fi
+
+
+   
